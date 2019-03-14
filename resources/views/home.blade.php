786ea4a15e6c5689c719eece957dbda920a77b81
Ferry <allan.ferry@orange.fr>
2019-03-14 14:14:39 +0100

    20

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,7 +30,7 @@
 <ul>
     
 @foreach($posts as $post)
-<li><a href='/articles/{{$post->post_name}}'>{{ $post->post_title }}</a></li>
+<li><a href='/articles/{{$post->post_name}}'>{{$post->post_title}}</a></li>
 @endforeach
 </ul>
 @endsection
